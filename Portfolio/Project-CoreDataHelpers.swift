ba6a930a512b4058cec1eabdbebc7b47fab4fe17
PaulJ <paulj@agylia.com>
2020-10-27 12:21:22 +0000

    Add CoreData helpers

---
+++ Portfolio/Project-CoreDataHelpers.swift
@@ -0,0 +1,66 @@
+//
+//  Project-CoreDataHelpers.swift
+//  Portfolio
+//
+//  Created by Paul Jackson on 27/10/2020.
+//
+
+import Foundation
+
+extension Project {
+    var projectTitle: String {
+        title ?? "New Project"
+    }
+    
+    var projectDetail: String {
+        detail ?? ""
+    }
+    
+    var projectColor: String {
+        color ?? "Light Blue"
+    }
+
+    var projectItems: [Item] {
+        let itemsArray = items?.allObjects as? [Item] ?? []
+        return itemsArray.sorted { first, second in
+            if first.completed == false {
+                if second.completed == true {
+                    return true
+                }
+            } else if first.completed == true {
+                if second.completed == false {
+                    return false
+                }
+            }
+            
+            if first.priority > second.priority {
+                return true
+            } else if first.priority < second.priority  {
+                return false
+            }
+            
+            return first.itemCreationDate < second.itemCreationDate
+        }
+    }
+    
+    var projectCompletion: Double {
+        let originalItems = items?.allObjects as? [Item] ?? []
+        guard !originalItems.isEmpty else { return 0 }
+    
+        let completedItems = originalItems.filter(\.completed)
+        return Double(completedItems.count) / Double(originalItems.count)
+    }
+
+    static var example: Project {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let project = Project(context: viewContext)
+        project.title = "Exmaple Project"
+        project.detail = "This is an example project"
+        project.closed = true
+        project.createdDate = Date()
+        
+        return project
+    }
+}
