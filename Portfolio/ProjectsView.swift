7f93cdae005f30d66099cf10cbe603f07a72a62e
PaulJ <paulj@agylia.com>
2020-11-10 15:15:37 +0000

    Add project item icons

--- Portfolio/ProjectsView.swift
+++ Portfolio/ProjectsView.swift
@@ -34,7 +34,7 @@ struct ProjectsView: View {
                 ForEach(projects.wrappedValue) { project in
                     Section(header: ProjectHeaderView(project: project)) {
                         ForEach(project.projectItems(using: sortDescriptor)) { item in
-                            ItemRowView(item: item)
+                            ItemRowView(project: project, item: item)
                         }
                         .onDelete { offsets in
                             let allItems = project.projectItems
