a836696960155c3871bbf58c3f6b43e158964e1d
PaulJ <paulj@agylia.com>
2020-11-13 18:27:09 +0000

    Add initial awards data and UI

---
+++ Portfolio/Award.swift
@@ -0,0 +1,21 @@
+//
+//  Award.swift
+//  Portfolio
+//
+//  Created by Paul Jackson on 13/11/2020.
+//
+
+import Foundation
+
+struct Award: Decodable, Identifiable {
+    var id: String { name }
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: Int
+    let image: String
+
+    static let allAwards = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+}
