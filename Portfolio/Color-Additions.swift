fc79f6505ab38a82a1ea4cae3587eb7b98c9799b
PaulJ <paulj@agylia.com>
2020-11-29 10:59:52 +0000

    Add home view implementation

---
+++ Portfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  Portfolio
+//
+//  Created by Paul Jackson on 24/11/2020.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
